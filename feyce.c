594ec1fb7b90e37e14571816f473f4e97c7f49d0
fara-bia <ulumuseceristan@gmail.com>
2026-07-09 21:54:15 +0300

    Update feyce.c

--- feyce.c
+++ feyce.c
@@ -2,7 +2,12 @@
     Copyright 2026 fara-bia
     SPDX-License-Identifier: Apache-2.0
  */
+#include <stdio.h>
+#include "mutuals.h"
 
 int main (int argc, char* argv[]) {
-
+    repeat(10) {
+        printf("Test Message\n");
+    }
+    return 0;
 }
