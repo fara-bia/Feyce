4cc8bcff9ec7bb190517971b5f496f111fe78dda
fara-bia <ulumuseceristan@gmail.com>
2026-07-10 14:57:57 +0300

    testing mutuals.h

--- feyce.c
+++ feyce.c
@@ -2,11 +2,10 @@
     Copyright 2026 fara-bia
     SPDX-License-Identifier: Apache-2.0
  */
-#include <stdio.h>
 #include "mutuals.h"
 
 int main (int argc, char* argv[]) {
-    repeat(10) {
+    repeat(atoi(argv[1])) {
         printf("Test Message\n");
     }
     return 0;
