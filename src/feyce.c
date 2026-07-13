d701b37e6a14ce673be50bc5f8e23e27993d8ef6
fara-bia <ulumuseceristan@gmail.com>
2026-07-13 20:07:32 +0300

    another commit

--- src/feyce.c
+++ src/feyce.c
@@ -3,14 +3,11 @@
     SPDX-License-Identifier: Apache-2.0
  */
 
-// development of this source file and its header is delayed
 #include <stdio.h>
-#include <stdlib.h>
 #include "mutuals.h"
 
 int main (int argc, char* argv[]) {
-    repeat(atoi(argv[1])) {
-        printf("Test Message\n");
-    }
+
+
     return 0;
 }
